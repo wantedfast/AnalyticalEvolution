d935ddf80b072e8698ac71277581845b3a967f5a
Geralt Wang(MSFT) <v-xinlwa@microsoft.com>
2023-06-19 14:06:44 +0900

    add new

--- Analytical Evolution/Analytical Evolution/Program.cs
+++ Analytical Evolution/Analytical Evolution/Program.cs
@@ -1,116 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Bytescout.Spreadsheet.Charts;
+using ComponentAce.Compression.Libs.zlib;
+using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System;
-using System.Runtime.ExceptionServices;
 using System.IO;
-using Bytescout.Spreadsheet;
-using System.ComponentModel;
+using AxisPosition = OxyPlot.Axes.AxisPosition;
 
-namespace AnalyticalEvolution
-{ 
-    class Program
+public class EquationCalculator
+{
+    public double CalculateH(double v, double pr, double M)
     {
-        // nvR Code (M=8, n=1,2,3,4)
-        static int m = 8;
-        //static int[] n = new int[] {1,2,3,4 };
+        double numerator = pr + (v - 1) * (1 - pr) / (M - 1);
+        //double denominator = M;
+        //return numerator / denominator;
 
-        // Prci | xi  0.14,0.15,0.99
-        static double[] pr = new double[1000];
+        return numerator;
+    }
 
-        static int dh = 8;
+    public double CalculateN(double v, double pr, double M)
+    {
+        double term1 = (1 - CalculateH(v, pr, M)) / CalculateH(v, pr, M);
+        double term2 = (1 - CalculateH(M - v, pr, M)) / CalculateH(M - v, pr, M);
 
-        // stupid data structure
-        static Dictionary<int, double> data = new Dictionary<int, double>();
+        return term1 * term2;
+    }
 
-        static Spreadsheet spreadsheet = new Spreadsheet();
-
-        static Worksheet worksheet = spreadsheet.Workbook.Worksheets.Add("AnalyticalEvolution");
-
-        static Queue<string> queue = new Queue<string>();
-
-
-        // this function to calc the h
-        public static double CalcH(double pr,int m, int v)
-        {
-            int temp = v - 1;
-
-            double last = (1 - pr) / (m - 1);
-
-            last = last * temp;
-
-            double result = last + pr;
-
-            return result;
-        }
-
-        public static void EquiPosteriorPro(string s, int v, double pr,int cellCount)
-        {
-            double h = CalcH(pr, m, v);
-            
-            //  this is A part
-            double firstPart = (1 - h) / h;
-            
-            // B part
-            double temp = CalcH(pr, m, m-v);
-            double secondPart = (1 - temp) / temp;
-             
-            //
-            double seta = firstPart*secondPart;
-
-            double r = Math.Sqrt(dh) * Math.Log(seta);
-            
-            r = r / 2;
-
-            worksheet.Cell(s + cellCount).Value = r;
-      
-            //cellCount++;
-        }
-        
-        // useless function
-        static void IntialArray()
-        {
-            pr[0] = 0.14;
-
-            int i = 1;
-            while (pr[i] <= 1)
-            {
-                pr[++i] = pr[i] + 0.14;
-            }
-        }
-
-        public static void Main()
-        {
-            queue.Enqueue("A");
-            queue.Enqueue("B");
-            queue.Enqueue("C");
-            queue.Enqueue("D");
-            
-            pr[0] = 0.14;
-            int j = 0;
-            int cellNumber = 1;
-
-            while(queue.Count()!=0)
-            {
-                string s = queue.Dequeue();
-                worksheet.Cell(s + 1).Value = cellNumber + "vrCode";
-                
-                int cellCount = 2;
-
-                // in this loop the pr will be changed
-                while (pr[j] <= 1)
-                {
-                    EquiPosteriorPro(s, cellNumber, pr[j], cellCount++);
-                    pr[j + 1] = pr[j] + 0.01;
-                    j++;
-                }
-
-                cellNumber++;
-                Array.Clear(pr, 1, 999);
-            }
-
-            spreadsheet.SaveAs(@"C:\Users\ITA\Desktop\task2.xlsx");
-
-            //WriteExcel(data);
-        }  
+    public double CalculateR(double dh, double v, double pr, double M)
+    {
+        double logN = Math.Log(CalculateN(v, pr, M));
+        return Math.Sqrt(dh) * logN / 2;
     }
 }
 
+public class Program
+{
+    public static void Main()
+    {
+
+        EquationCalculator p = new EquationCalculator();
+
+        List<double> Pr = new List<double>();
+
+        for (double pr = 0.14; pr <= 0.99; pr += 0.01)
+        {
+            Pr.Add(pr);
+        }
+
+        List<double> R = new List<double>();
+
+        Stack<int> dh = new Stack<int>();
+
+        dh.Push(64);
+        dh.Push(20);
+        dh.Push(30);
+        dh.Push(12);
+        dh.Push(2);
+        var plotModel = new PlotModel();
+
+        // Create X and Y axes
+        var xAxis = new LinearAxis { Position = AxisPosition.Bottom, Title = "Pr", Minimum = 0, Maximum = 1.2, MajorStep = 0.2 };
+        var yAxis = new LinearAxis { Position = AxisPosition.Left, Title = "R", Minimum = -40, Maximum = 0, MajorStep = 10 };
+
+        // Add axes to the plot model
+        plotModel.Axes.Add(xAxis);
+        plotModel.Axes.Add(yAxis);
+        for (int v = 1; v <= 5; v++)
+        {
+            var d = dh.Pop();
+
+            for (int i = 0; i < Pr.Count; i++)
+            {
+                R.Add(p.CalculateR(d, v, Pr[i], 8));
+            }
+
+            string s = v + "vrCode";
+            var lineAnnotation = new LineAnnotation
+            {
+                Text = s
+            };
+            plotModel.Annotations.Add(lineAnnotation);  
+            Plot(Pr, R, plotModel);
+            R.Clear();
+        }
+    }
+
+    static void Plot(List<double> Pr, List<double> R, PlotModel plotModel)
+    {
+    
+
+        // Create a line series and add data points
+        var series = new LineSeries();
+        
+        for (int i = 0; i < Pr.Count; i++)
+        {
+            series.Points.Add(new DataPoint(Pr[i], R[i]));
+        }
+
+        // Add the series to the plot model
+        plotModel.Series.Add(series);
+
+        // Create a PDF exporter with modified document size
+        var pdfExporter = new PdfExporter { Width = 800, Height = 600 };
+        // Export the plot model to a PDF file
+        if (File.Exists("output.pdf"))
+        {
+            using (var str = File.Open("output.pdf", FileMode.Open, FileAccess.Write))
+            {
+
+                pdfExporter.Export(plotModel, str);
+            }
+
+        }
+
+        else
+
+
+        {
+            using (var stream = System.IO.File.Create("output.pdf"))
+            {
+                pdfExporter.Export(plotModel, stream);
+            }
+        }
+      
+
+        Console.WriteLine("Plot saved as output.pdf");
+    }
+}
