79bb363127e577adb5cc2fb2f831188374e7c1f2
Geralt Wang(MSFT) <inaccuarcy@gmail.com>
2023-06-20 16:04:27 +0900

    Final version
    everything looks okay

--- Analytical Evolution/Analytical Evolution/Program.cs
+++ Analytical Evolution/Analytical Evolution/Program.cs
@@ -5,7 +5,9 @@ using OxyPlot.Annotations;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 using System;
+using System.Drawing.Text;
 using System.IO;
+using System.Reflection.PortableExecutable;
 using AxisPosition = OxyPlot.Axes.AxisPosition;
 
 public class EquationCalculator
@@ -32,12 +34,29 @@ public class EquationCalculator
         double logN = Math.Log(CalculateN(v, pr, M));
         return Math.Sqrt(dh) * logN / 2;
     }
+
+    public Queue<OxyColor> colors = new Queue<OxyColor>();
+
+    public EquationCalculator()
+    {
+        colors.Enqueue(OxyColors.Blue);
+
+        colors.Enqueue(OxyColors.Orange);
+
+        colors.Enqueue(OxyColors.Green);
+
+        colors.Enqueue(OxyColors.Red);
+
+        colors.Enqueue(OxyColors.Black);
+    }
 }
 
 public class Program
 {
     public static void Main()
     {
+
+        int M = 5;
 
         EquationCalculator p = new EquationCalculator();
 
@@ -52,16 +71,16 @@ public class Program
 
         Stack<int> dh = new Stack<int>();
 
-        dh.Push(64);
-        dh.Push(20);
-        dh.Push(30);
-        dh.Push(12);
-        dh.Push(2);
+        dh.Push(64); // exhastive
+        dh.Push(20); // 4vr
+        dh.Push(30); // 3vr
+        dh.Push(3); // 2vr
+        dh.Push(2); // 1vr
         var plotModel = new PlotModel();
 
         // Create X and Y axes
-        var xAxis = new LinearAxis { Position = AxisPosition.Bottom, Title = "Pr", Minimum = 0, Maximum = 1.2, MajorStep = 0.2 };
-        var yAxis = new LinearAxis { Position = AxisPosition.Left, Title = "R", Minimum = -40, Maximum = 0, MajorStep = 10 };
+        var xAxis = new LinearAxis { Position = AxisPosition.Bottom, Title = "Pr when M equals " + M, Minimum = 0, Maximum = 1.1, MajorStep = 0.2 };
+        var yAxis = new LinearAxis { Position = AxisPosition.Left, Title = "Evaluation function", Minimum = -40, Maximum = 0, MajorStep = 10 };
 
         // Add axes to the plot model
         plotModel.Axes.Add(xAxis);
@@ -72,37 +91,45 @@ public class Program
 
             for (int i = 0; i < Pr.Count; i++)
             {
-                R.Add(p.CalculateR(d, v, Pr[i], 8));
+                R.Add(p.CalculateR(d, v, Pr[i], M));
             }
 
             string s = v + "vrCode";
-            var lineAnnotation = new LineAnnotation
-            {
-                Text = s
-            };
-            plotModel.Annotations.Add(lineAnnotation);  
-            Plot(Pr, R, plotModel);
+            OxyColor color = p.colors.Dequeue();
+
+            Plot(Pr,R, plotModel, s, color);
             R.Clear();
         }
     }
 
-    static void Plot(List<double> Pr, List<double> R, PlotModel plotModel)
+    static void Plot(List<double> Pr, List<double> R, PlotModel plotModel, string s, OxyColor color)
     {
-    
-
         // Create a line series and add data points
         var series = new LineSeries();
-        
+
+        series.Color = color;
+
         for (int i = 0; i < Pr.Count; i++)
         {
             series.Points.Add(new DataPoint(Pr[i], R[i]));
         }
 
+        var lineAnnotation = new TextAnnotation
+        {
+            Text = s,
+            TextPosition = new DataPoint(series.Points.Min(p => p.X), series.Points.Min(p => p.Y)),
+            TextColor = color
+            //TextHorizontalAlignment = HorizontalAlignment.Left,
+            //TextVerticalAlignment = VerticalAlignment.Bottom
+        };
+
+        plotModel.Annotations.Add(lineAnnotation);
+
         // Add the series to the plot model
         plotModel.Series.Add(series);
 
         // Create a PDF exporter with modified document size
-        var pdfExporter = new PdfExporter { Width = 800, Height = 600 };
+        var pdfExporter = new PdfExporter { Width = 800, Height = 500 };
         // Export the plot model to a PDF file
         if (File.Exists("output.pdf"))
         {
@@ -111,20 +138,13 @@ public class Program
 
                 pdfExporter.Export(plotModel, str);
             }
-
         }
-
         else
-
-
         {
             using (var stream = System.IO.File.Create("output.pdf"))
             {
                 pdfExporter.Export(plotModel, stream);
             }
         }
-      
-
-        Console.WriteLine("Plot saved as output.pdf");
     }
 }
